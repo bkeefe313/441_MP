99a982bbe35ccfd67ecbbe8c66dc8874999a2a1e
Jimmy Truong <jimmytruong@mines.edu>
2022-10-10 17:33:43 -0600

    added arthur obj

--- ClutchNorris-a3/cmake-build-debug/cmake_install.cmake
+++ ClutchNorris-a3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jjarroyo/Library/CloudStorage/OneDrive-Personal/Documents/Fall Semester 2022/Computer Graphics/a3
+# Install script for directory: /Users/jimmy/Documents/Code Shit/441_MP/ClutchNorris-a3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jjarroyo/Library/CloudStorage/OneDrive-Personal/Documents/Fall Semester 2022/Computer Graphics/a3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/jimmy/Documents/Code Shit/441_MP/ClutchNorris-a3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
